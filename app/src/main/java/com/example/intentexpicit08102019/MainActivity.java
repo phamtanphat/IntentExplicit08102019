9bff4cbc8d0453bb94b925f78e1be1684c16b9c2
phatdroid94 <phatdroid94@gmail.com>
2019-11-26 20:26:58 +0700

    init

---
+++ app/src/main/java/com/example/intentexpicit08102019/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.intentexpicit08102019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnIntentString;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnIntentString = findViewById(R.id.buttonIntentString);
+
+        btnIntentString.setOnClickListener(v -> {
+            // statement
+            Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+            intent.putExtra("chuoi","Hello main 2");
+            startActivity(intent);
+        });
+    }
+}
