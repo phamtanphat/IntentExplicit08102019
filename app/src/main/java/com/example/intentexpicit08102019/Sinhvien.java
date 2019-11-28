9a0b6f480cae1ec9eff17dcd24b79543e4323fdf
phatdroid94 <phatdroid94@gmail.com>
2019-11-28 20:16:42 +0700

    parcel

--- app/src/main/java/com/example/intentexpicit08102019/Sinhvien.java
+++ app/src/main/java/com/example/intentexpicit08102019/Sinhvien.java
@@ -1,13 +1,42 @@
 package com.example.intentexpicit08102019;
 
-import java.io.Serializable;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class Sinhvien implements Serializable {
+public class Sinhvien implements Parcelable {
     public String name;
     public String age;
 
     public Sinhvien(String name, String age) {
         this.name = name;
         this.age = age;
+    }
+
+    protected Sinhvien(Parcel in) {
+        name = in.readString();
+        age = in.readString();
+    }
+
+    public static final Creator<Sinhvien> CREATOR = new Creator<Sinhvien>() {
+        @Override
+        public Sinhvien createFromParcel(Parcel in) {
+            return new Sinhvien(in);
+        }
+
+        @Override
+        public Sinhvien[] newArray(int size) {
+            return new Sinhvien[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(age);
     }
 }
